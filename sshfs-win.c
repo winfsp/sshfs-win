674c3643936497de1eca74c556abb1aa3e92d9d8
Bill Zissimopoulos <billziss@navimatics.com>
2016-07-14 22:56:48 -0700

    sshfs-win.c: improve command line handling

--- sshfs-win.c
+++ sshfs-win.c
@@ -1,4 +1,6 @@
+#include <stdarg.h>
 #include <stdio.h>
+#include <pwd.h>
 #include <unistd.h>
 
 #define SSHFS_ARGS                      \
@@ -8,6 +10,21 @@
     "-oUserKnownHostsFile=/dev/null",   \
     "-oStrictHostKeyChecking=no"
 
+#if 0
+#define execle pr_execl
+static void pr_execl(const char *path, ...)
+{
+    va_list ap;
+    const char *arg;
+
+    va_start(ap, path);
+    fprintf(stderr, "%s\n", path);
+    while (0 != (arg = va_arg(ap, const char *)))
+        fprintf(stderr, "    %s\n", arg);
+    va_end(ap);
+}
+#endif
+
 int main(int argc, char *argv[])
 {
     static const char *sshfs = "/bin/sshfs.exe";
@@ -15,32 +32,57 @@ int main(int argc, char *argv[])
     {
         "PATH=/bin",
     };
-    char idmap[64], volpfx[256], remote[256], *p, *q;
+    struct passwd *passwd;
+    char idmap[64], volpfx[256], remote[256];
+    char *locuser, *userhost, *path, *p;
 
     if (3 != argc)
         return 2;
 
-    snprintf(idmap, sizeof idmap, "-ouid=%d,gid=%d", -1, -1);
-
     snprintf(volpfx, sizeof volpfx, "--VolumePrefix=%s", argv[1]);
 
-    p = argv[1];
-    while ('\\' == *p)
-        p++;
-    while (*p && '\\' != *p)
-        p++;
-    while ('\\' == *p)
-        p++;
-    q = p;
-    while (*q && '\\' != *q)
-        q++;
-    snprintf(remote, sizeof remote, "%.*s:%s", q - p, p, *q ? q + 1 : q);
-    p = remote;
-    while (*p)
-    {
+    /* translate backslash to forward slash */
+    for (p = argv[1]; *p; p++)
         if ('\\' == *p)
             *p = '/';
+
+    /* skip class name (\\sshfs\) */
+    p = argv[1];
+    while ('/' == *p)
         p++;
+    while (*p && '/' != *p)
+        p++;
+    while ('/' == *p)
+        p++;
+
+    /* parse instance name (syntax: [locuser=]user@host) */
+    locuser = userhost = p;
+    while (*p && '/' != *p)
+    {
+        if ('=' == *p)
+            userhost = p + 1;
+        p++;
+    }
+    if (*p)
+        *p++ = '\0';
+    path = p;
+
+    snprintf(remote, sizeof remote, "%s:%s", userhost, path);
+
+    /* get local user name */
+    p = locuser;
+    while (*p && '@' != *p && '=' != *p)
+        p++;
+
+    snprintf(idmap, sizeof idmap, "-ouid=-1,gid=-1");
+    if (*p)
+    {
+        *p = '\0';
+
+        /* get uid/gid from local user name */
+        passwd = getpwnam(locuser);
+        if (0 != passwd)
+            snprintf(idmap, sizeof idmap, "-ouid=%d,gid=%d", passwd->pw_uid, passwd->pw_gid);
     }
 
     execle(sshfs, sshfs, SSHFS_ARGS, idmap, volpfx, remote, argv[2], (void *)0, environ);
